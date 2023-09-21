b9d2961a0fbfca1c1aa7df01764049ae1e3179b2
Dominic Cummings <dominiccummings95@gmail.com>
2023-09-21 12:02:50 -0500

    Starting on header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 import './header.css';
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">Let's Build Something Amazing with GPT-3 OpenAi</h1>
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder="Your Email Address"/>
+          <button type="button">Get Started</button>
+        </div>
+      </div>
     </div>
   )
 }
