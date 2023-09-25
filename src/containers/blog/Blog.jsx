5de6f11215090a3aaa1d5ddfc35c2e99df822b1f
Dominic Cummings <dominiccummings95@gmail.com>
2023-09-25 13:27:00 -0500

    Blog finished

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -5,7 +5,7 @@ import './blog.css';
 const Blog = () => {
   return (
     <div className="gpt3__blog section__padding" id='blog'>
-      <div className="gpt3__blog-header">
+      <div className="gpt3__blog-heading">
         <h1 className='gradient__text'>A lot is happening, 
         We are blogging about it.</h1>
       </div>
@@ -15,9 +15,9 @@ const Blog = () => {
         </div>
         <div className="gpt3__blog-container_groupB">
           <Article imgUrl={blog02} date="Sep 20, 2023" title="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
-          <Article imgUrl={blog03} date="Sep 20, 2023" titl="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
-          <Article imgUrl={blog04} date="Sep 20, 2023" titl="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
-          <Article imgUrl={blog05} date="Sep 20, 2023" titl="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+          <Article imgUrl={blog03} date="Sep 20, 2023" title="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+          <Article imgUrl={blog04} date="Sep 20, 2023" title="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+          <Article imgUrl={blog05} date="Sep 20, 2023" title="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
         </div>
       </div>
     </div>
