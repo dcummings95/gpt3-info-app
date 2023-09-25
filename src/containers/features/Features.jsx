bec68dbba2304a74dcf12d785a94617a004a7501
Dominic Cummings <dominiccummings95@gmail.com>
2023-09-25 12:07:53 -0500

    2nd page done

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import './features.css';
 const Features = () => {
   return (
-    <div>
-      Features
+    <div className="gpt3__features section__padding" id='features'>
+      <div className="gpt3__features-heading">
+        <h1 className="gradient__text">The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.</h1>
+      </div>
     </div>
   )
 }
